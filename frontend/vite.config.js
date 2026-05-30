bb40ff9250c29ed79946c94b3c106aadff2a1f99
hanjangweb <omitterang@gmail.com>
2026-05-30 14:53:44 +0530

    performance update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -27,5 +27,17 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react';
+            if (id.includes('framer-motion')) return 'framer';
+            if (id.includes('lucide') || id.includes('toast')) return 'ui';
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
 })
